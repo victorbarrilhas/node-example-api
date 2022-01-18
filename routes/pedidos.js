d9e8a416a0431d2c1224a860dd582bbda6d2b7d9
Victor Barrilhas <vbarrilhas@gmail.com>
2022-01-18 13:14:22 -0300

    Consultando várias tabelas (INNER JOIN)

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -8,30 +8,43 @@ router.get("/", (req, res, next) => {
         if (error) {
             return res.status(500).send({ error: error });
         }
-        conn.query("SELECT * FROM pedidos;", (error, result, fields) => {
-            if (error) {
-                return res.status(500).send({ error: error });
+        conn.query(
+            `SELECT pedidos.id_pedido,
+                           pedidos.quantidade,
+                           produtos.id_produto,
+                           produtos.nome,
+                           produtos.preco
+                      FROM pedidos
+                INNER JOIN produtos
+                        ON produtos.id_produto = pedidos.id_produto;`,
+            (error, result, fields) => {
+                if (error) {
+                    return res.status(500).send({ error: error });
+                }
+                const response = {
+                    pedidos: result.map((pedido) => {
+                        return {
+                            id_pedido: pedido.id_pedido,
+                            quantidade: pedido.quantidade,
+                            produto: {
+                                id_produto: pedido.id_produto,
+                                nome: pedido.nome,
+                                preco: pedido.preco,
+                            },
+                            request: {
+                                tipo: "GET",
+                                descricao:
+                                    "Retorna os detalhes de um pedido específico",
+                                url:
+                                    "http://localhost:3000/pedidos/" +
+                                    pedido.id_pedido, //ToDo: extrair para variavel de ambiente.
+                            },
+                        };
+                    }),
+                };
+                return res.status(200).send(response);
             }
-            const response = {
-                quantidade: result.length,
-                produtos: result.map((pedido) => {
-                    return {
-                        id_pedido: pedido.id_pedido,
-                        id_produto: pedido.id_produto,
-                        quantidade: pedido.quantidade,
-                        request: {
-                            tipo: "GET",
-                            descricao:
-                                "Retorna os detalhes de um pedido específico",
-                            url:
-                                "http://localhost:3000/pedidos/" +
-                                pedido.id_pedido, //ToDo: extrair para variavel de ambiente.
-                        },
-                    };
-                }),
-            };
-            return res.status(200).send(response);
-        });
+        );
     });
 });
 
