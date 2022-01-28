74b8a7d18a1dbf22529db1b158e85f92a4798eeb
Victor Barrilhas <vbarrilhas@gmail.com>
2022-01-28 00:17:26 -0300

    Cadastro de usuario (com senha HASH)

--- SCRIPTS/create_tables.sql
+++ SCRIPTS/create_tables.sql
@@ -1,9 +1,3 @@
-CREATE TABLE `produtos` (
-  `id_produto` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(45) NOT NULL,
-  `preco` float NOT NULL,
-  PRIMARY KEY (`id_produto`)
-) ENGINE = InnoDB AUTO_INCREMENT = 23 DEFAULT CHARSET = utf8mb3;
 CREATE TABLE `pedidos` (
   `id_pedido` int NOT NULL AUTO_INCREMENT,
   `id_produto` int NOT NULL,
@@ -11,6 +5,20 @@ CREATE TABLE `pedidos` (
   PRIMARY KEY (`id_pedido`),
   KEY `fk_pedidos_produtos_idx` (`id_produto`),
   CONSTRAINT `fk_pedidos_produtos` FOREIGN KEY (`id_produto`) REFERENCES `produtos` (`id_produto`)
-) ENGINE = InnoDB AUTO_INCREMENT = 4 DEFAULT CHARSET = utf8mb3;
-ALTER TABLE produtos
-ADD COLUMN imagem_produto VARCHAR(500);
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `produtos` (
+  `id_produto` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) NOT NULL,
+  `preco` float NOT NULL,
+  `imagem_produto` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id_produto`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `senha` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_usuario`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
